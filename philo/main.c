ed39f21e38a00fabeeee69190efd396969818442
0ac_raf0dev <achrafennadiri@gmail.com>
2025-07-25 14:36:12 +0100

    the parcing skip the tests as expacted

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 15:19:20 by acennadi          #+#    #+#             */
-/*   Updated: 2025/07/25 11:32:27 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/07/25 14:33:13 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,22 @@ int	ft_isdigit(int c)
 		return (1);
 	return (0);
 }
-int is_all_digit()
+int ft_is_all_digit(char *str)
 {
-    
+    int i;
+
+    i = 0;
+    if(!str[i])
+        return (0);
+    if (str[0] == '+')
+        i++;
+    while (str[i])
+    {
+        if (!ft_isdigit(str[i]))
+            return (0);
+        i++;
+    }
+    return (1);
 }
 
 int	ft_atoi(const char *str)
@@ -50,7 +63,7 @@ int	ft_atoi(const char *str)
 	return (sign * res);
 }
 
-void puterror(int errnum)
+int puterror(int errnum)
 {
     if (errnum == 0)
         printf("Expect: [num_philo] [t_die] [t_eat] [t_sleep] [optional: must_eat_count]\n");
@@ -58,20 +71,20 @@ void puterror(int errnum)
         printf("Error: all arguments must be positive integers\n");
     if (errnum == 2)
         printf("Error: \n");
+    return 0;
 }
 int philo_parcer(int ac, char **av)
 {
     int i;
-    int status;
-
+    
     i = 1;
+    // 5 or 6 args this is optional
     if (ac != 6 && ac != 5)
-        return (puterror(0), 0);
+        return (puterror(0));
     while (i < ac)
     {
-        status = ft_atoi(av[i]);
-        if (!status)
-            return (puterror(1), 0);
+        if (!ft_is_all_digit(av[i]))
+            return (puterror(1));
         i++;
     }
     return 1;
