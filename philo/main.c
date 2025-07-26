d63edfd08cd80af96ea4e65a3b61b179fc994493
0ac_raf0dev <achrafennadiri@gmail.com>
2025-07-26 14:49:04 +0100

    .

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 15:19:20 by acennadi          #+#    #+#             */
-/*   Updated: 2025/07/26 14:44:43 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/07/26 14:48:32 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,6 @@ int	main(int ac, char **av)
 	status = philo_parcer(ac, av, &data);
 	if (!status)
 		return (0);
-	printf("Test skip Parcing!, and this is the result of the data\n");
-	printf("this is the result of struct: \n%d\n%d\n%d\n%d\n%d\n",
-		data.number_of_philosophers, data.time_to_die, data.time_to_eat,
-		data.time_to_sleep, data.number_of_times_each_philosopher_must_eat);
+    
 	return (0);
 }
