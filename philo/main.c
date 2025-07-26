ffdaefc238df7e91bcb0e07ceab9d41c195cebff
0ac_raf0dev <achrafennadiri@gmail.com>
2025-07-26 16:22:40 +0100

    add some roles

--- philo/main.c
+++ philo/main.c
@@ -6,19 +6,26 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 15:19:20 by acennadi          #+#    #+#             */
-/*   Updated: 2025/07/26 15:42:20 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/07/26 16:21:29 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <pthread.h>
 
 void philo_init(t_configuration *data)
 {
+    int i;
     t_phios *fork;
     
-    fork = malloc(sizeof(pthread_mutex_t) * fork->config->number_of_philosophers);
+    i = 0;
+    fork = malloc(sizeof(pthread_mutex_t) * data->number_of_philosophers);
     if (!fork)
         t_clean(fork);
+    while (i < data->number_of_philosophers) {
+        pthread_mutex_init(&data->forks[i], NULL);
+        i++;
+    }
 }
 int	main(int ac, char **av)
 {
